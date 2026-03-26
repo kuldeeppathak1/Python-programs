e391f0001212b454abf8d4a23e24ab1987357dae
Kuldeep <pathakkuldeep1988@gmail.com>
2026-03-26 22:59:59 +0530

    duplicate

--- second_largest_element.py
+++ second_largest_element.py
@@ -8,3 +8,23 @@ def second_largest(nums):
 # Example
 numbers = [10, 20, 4, 45, 99, 99]
 print("Second largest:", second_largest(numbers))
+
+############################
+def second_largest1(arr):
+    if len(arr) < 2:
+        return None
+    
+    first = second = float('-inf')
+    
+    for num in arr:
+        if num > first:
+            second = first
+            first = num
+        elif num > second and num != first:
+            second = num
+    
+    return second if second != float('-inf') else None
+    
+    
+numbers = [10, 20, 4, 45, 99, 99]
+print(second_largest1(numbers))
